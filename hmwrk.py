49c606fc06a41fcdc481bdb19240e29793c9dde5
Superdolphin123 <130215364+Superdolphin123@users.noreply.github.com>
2024-08-03 17:15:22 +0600

    Add files via upload

---
+++ hmwrk.py
@@ -0,0 +1,14 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(99.9>99.8 and 1000 != 1000.1)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+#4th program
+s = '123.456'
+s = 123.456
+s = int(s*10)
+s = s%10
+print(s)
