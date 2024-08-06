bd7a9e5047dccd5cf8e877bceeffe9b647b28da8
Superdolphin123 <130215364+Superdolphin123@users.noreply.github.com>
2024-08-06 22:44:56 +0600

    Add files via upload

---
+++ hmwrk2.py
@@ -0,0 +1,5 @@
+nmbr_of_hmwrk = 12
+hrs = 1.5
+lesson_name = 'Python'
+tasktime = hrs / nmbr_of_hmwrk
+print('Курс:',lesson_name,'Всего задач:',nmbr_of_hmwrk,'Затрачено часов:',hrs,'Среднее время выполнения:', tasktime)
