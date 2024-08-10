61c631265ae9627f77f27aaa494696eeb0ce1a61
Superdolphin123 <130215364+Superdolphin123@users.noreply.github.com>
2024-08-10 21:18:40 +0600

    Update module_1_7.py
    update code

--- module_1_7.py
+++ module_1_7.py
@@ -1,10 +1,8 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 gpa = {}
-students = sorted(list(students))
+students = sorted(students)
 for i in range(len(grades)):
     grades[i] = sum(grades[i]) / len(grades[i])
-for i in range(len(students)):
-    for j in range(len(grades)):
-        gpa.update({students[i]: grades[j]})
+    gpa.update({students[i]: grades[i]})
 print(gpa)
