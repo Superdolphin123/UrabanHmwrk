4b7b5b7dde82e16c2ad071223e7b4a0537f5d10f
Superdolphin123 <130215364+Superdolphin123@users.noreply.github.com>
2024-12-08 19:10:56 +0600

    Add files via upload

---
+++ python_introduction/hmwrk.py
@@ -0,0 +1,15 @@
+# 1st program
+print(9**0.5 * 5)
+
+# 2nd program
+print(9.98 < 9.99 and 1000 != 1001)
+
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+# 4th program
+s = float('123.456')
+s *= 10
+print(int(s % 10))
